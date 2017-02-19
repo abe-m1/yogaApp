b423e5b9c7b15383d3ce9ddba9a746060defb934
abe <abestbet1@gmail.com>
2017-02-19 14:48:14 -0800

    set up route to home page

---
+++ src/core/configs/routes.config.js
@@ -0,0 +1,25 @@
+      (function () {
+        'use strict';
+
+        angular
+          .module('YogaApp.Configs')
+          .config(routesProvider);
+
+        function routesProvider($stateProvider, $urlRouterProvider) {
+          $stateProvider
+
+          // Employer States
+          .state('home', {
+            url: '/',
+            templateUrl: 'components/home/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'home'
+          })
+
+          
+
+          $urlRouterProvider.otherwise('/');
+        }
+
+
+      })();
