9ca96a9895267e6f51d7ddb2a9be61fa1ca4a3a0
abe <abestbet1@gmail.com>
2017-02-19 14:44:13 -0800

    app.modules.js

---
+++ src/core/modules/app.modules.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict'
+
+  angular
+    .module('YogaApp', ['YogaApp.Controllers',
+                      'YogaApp.Services',
+                      'YogaApp.Configs',
+                      'YogaApp.Directives',
+                      'YogaApp.Constants',
+                      'YogaApp.Filters',
+                      'ui.router'])
+
+    .run(function() {
+      
+    })
+      
+})();
