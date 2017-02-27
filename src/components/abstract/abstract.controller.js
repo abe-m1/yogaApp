ce45e494a447c766c5dc0ba2df9044f4e134842d
abe <abestbet1@gmail.com>
2017-02-27 10:37:19 -0800

    create object in abstractController to iterate

--- src/components/abstract/abstract.controller.js
+++ src/components/abstract/abstract.controller.js
@@ -9,6 +9,33 @@
 
     var vm = this
     console.log('i am working')
+
+      vm.bulletPoint = {
+       1: {
+         'text' : 'this will describe idea 1',
+         'link' : 'http://lorempixel.com/400/200/nature/',
+       },
+       2: {
+         'text' : 'this will describe idea 2' ,
+         'link' : 'http://lorempixel.com/400/200/nature/',
+       },
+       3: {
+         'text' : 'this will describe idea 3',
+         'link' : 'http://lorempixel.com/400/200/nature/',
+       },
+       4: {
+         'text' : 'this will describe idea 4',
+         'link' : 'http://lorempixel.com/400/200/nature/',
+       },
+       5: {
+         'text' : 'this will describe idea 5',
+         'link' : 'http://lorempixel.com/400/200/nature/',
+       },
+       6: {
+         'text' : 'this will describe idea 6',
+         'link' : 'http://lorempixel.com/400/200/nature/',
+       }
+     };
    
 
   }
