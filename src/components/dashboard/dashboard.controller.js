e8af4705f93eb435a74ddd66249c106b1291054a
abe <abestbet1@gmail.com>
2017-02-28 16:18:38 -0800

    create dashboard controller

---
+++ src/components/dashboard/dashboard.controller.js
@@ -0,0 +1,23 @@
+(function(module) {
+
+  "use strict";
+  var DashboardCtrl;
+  DashboardCtrl.$inject = [];
+
+
+
+  function DashboardCtrl() {
+    
+   
+
+    var vm = this;
+    console.log('i am hooked up crm')
+  
+
+  }
+
+
+  module.controller('DashboardCtrl', DashboardCtrl);
+
+})(angular.module('YogaApp.Controllers'));
+
