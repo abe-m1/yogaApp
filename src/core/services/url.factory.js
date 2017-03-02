987b2c3c38eccd2abff70f5cc3bc85de7da129eb
abe <abestbet1@gmail.com>
2017-03-02 11:12:49 -0800

    boilerplate for url.factory.js

---
+++ src/core/services/url.factory.js
@@ -0,0 +1,9 @@
+(function (module){
+    urlFactory.$inject = []
+
+    function urlFactory(){
+        return 
+    }
+
+    module.factory('urlFactory', urlFactory)
+}(angular.module('YogaApp.Services')))
