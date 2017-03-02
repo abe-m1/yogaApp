c8b8e3f598f6eaaf262dd095d100164de2a93f7e
abe <abestbet1@gmail.com>
2017-03-02 11:26:56 -0800

    configure token in header

--- src/core/services/tokenSigning.service.js
+++ src/core/services/tokenSigning.service.js
@@ -4,6 +4,18 @@
 
   function tokenSigningService(tokenService, $log) {
 
+      function request(config){
+          if (tokenService.retrieve('yoga_token')){
+              config.headers['token'] = tokenService.retrieve('yoga_token')
+          }
+
+          return config
+      }
+
+      return {
+          request: request
+      }
+
       
   }
   module.factory('tokenSigningService', tokenSigningService);
