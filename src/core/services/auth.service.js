493a0d310347687ffbd627d2ed3834e78893951d
abe <abestbet1@gmail.com>
2017-03-01 17:44:16 -0800

    auth service template

---
+++ src/core/services/auth.service.js
@@ -0,0 +1,18 @@
+(function(module){
+    'use strict'
+    var authService;
+
+    authService.$inject = [];
+
+    function authService() {
+        var exports = {
+           
+        }
+
+       
+
+       
+        return exports;
+    }
+    module.factory('authService', authService)
+})(angular.module('YogaApp.Services'))
