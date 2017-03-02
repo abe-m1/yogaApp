cad8a6c976adbd0b9354554df690277c107a18b8
abe <abestbet1@gmail.com>
2017-03-02 11:00:43 -0800

    boilerplate for token service

---
+++ src/core/services/token.service.js
@@ -0,0 +1,16 @@
+(function (module){
+    var tokenService;
+    tokenService.$inject = [];
+    function tokenService() {
+
+        var exports = {
+            
+        }
+
+      
+
+        return exports
+    }
+
+    module.factory("tokenService", tokenService)
+}(angular.module('YogaApp.Services')))
