28fb64d30c53cbf233f1b274215d1adc90f09992
abe <abestbet1@gmail.com>
2017-03-01 17:42:31 -0800

    login functionality in login controller

--- src/components/login/login.controller.js
+++ src/components/login/login.controller.js
@@ -9,7 +9,42 @@
   function LoginCtrl(authService, tokenService, TOKEN_HASH, CURRENT_USER, $state, localStorageManager) {
     
    console.log('login running')
+   var vm = this;
+    vm.loginData = {
+      email: '',
+      password: ''
+    }
 
+    
+    vm.error = ""
+    vm.hello = 'hello'
+    vm.login = function() {
+        console.log('button pressed')
+
+
+      
+
+      vm.data = { 'email': vm.loginData.email, 'password': vm.loginData.password };
+      authService.logIn(vm.data)
+        .then(function(data) {
+            console.log(data.data.token)
+            console.log('here 1')
+            localStorageManager.store(CURRENT_USER, data.data);
+
+          tokenService.store(TOKEN_HASH, data.data.token);
+        //   return userService.getUser(data.data.token);
+        })
+        .then(function(user) {
+          console.log('here2')
+          $state.go('dashboard');
+        
+
+        })
+        .catch(function(err) {
+          
+        })
+    }
+  
    
   
 
