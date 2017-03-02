8e2e10de8fbb5e5696bdf42e236bf6b32cfff388
abe <abestbet1@gmail.com>
2017-03-01 17:33:50 -0800

    create login controller

---
+++ src/components/login/login.controller.js
@@ -0,0 +1,21 @@
+(function(module) {
+
+  "use strict";
+  var LoginCtrl;
+  LoginCtrl.$inject = [];
+
+
+
+  function LoginCtrl() {
+    
+   console.log('login running')
+
+   
+  
+
+  }
+
+
+  module.controller('LoginCtrl', LoginCtrl);
+
+})(angular.module('YogaApp.Controllers'));
