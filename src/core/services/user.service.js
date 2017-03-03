25a9f3ec8aa82a92b894a0fbac0d63aac33d3382
abe <abestbet1@gmail.com>
2017-03-03 10:08:48 -0800

    boilerplate for user service

---
+++ src/core/services/user.service.js
@@ -0,0 +1,18 @@
+(function(module){
+    'use strict'
+
+    var userService;
+    userService.$inject = [];
+
+    function userService() {
+
+        var exports = {
+          
+        }
+
+
+        return exports
+    }
+
+    module.factory("userService", userService)
+})(angular.module('YogaApp.Services'))
