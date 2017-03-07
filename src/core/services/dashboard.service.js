9fc9d9418dc66377b533ae1707027354be498299
abe <abestbet1@gmail.com>
2017-03-06 18:43:40 -0800

    create dashboard service

---
+++ src/core/services/dashboard.service.js
@@ -0,0 +1,20 @@
+(function(module){
+    'use strict'
+
+    var dashboardService;
+    dashboardService.$inject = [];
+
+    function dashboardService() {
+
+        var exports = {
+          
+        }
+
+        
+
+
+        return exports
+    }
+
+    module.factory("dashboardService", dashboardService)
+})(angular.module('YogaApp.Services'))
