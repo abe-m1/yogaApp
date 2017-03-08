b5d37173ba93056667b314658814a2b260a8f403
abe <abestbet1@gmail.com>
2017-03-08 13:50:41 -0800

    create service for spinner

---
+++ src/core/services/spinner.service.js
@@ -0,0 +1,15 @@
+(function(module) {
+  var spinnerService;
+  spinnerService.$inject = [];
+
+  function loaderService() {
+
+    var exports = {
+      
+    }
+
+
+ return exports;
+  };
+  module.factory('spinnerService', spinnerService);
+}(angular.module("YogaApp.Services")));
