c3031fa25e5998750b718fcca76d72bca8b03941
abe <abestbet1@gmail.com>
2017-03-08 13:57:42 -0800

    boilerplate for spinner directive

---
+++ src/core/directives/spinner.directive.js
@@ -0,0 +1,19 @@
+(function(module) {
+  "use strict";
+
+  yogaSpinner.$inject = [];
+
+  function yogaSpinner() {
+
+
+
+    return {
+     
+    };
+
+  };
+
+
+  module.directive('yogaSpinner', yogaSpinner);
+
+})(angular.module('YogaApp.Directives'));
