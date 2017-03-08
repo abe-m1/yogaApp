9b254f46f990596db5c6c432a5e88d7bf18d21de
abe <abestbet1@gmail.com>
2017-03-08 14:03:35 -0800

    directive functionality to show hide spinner

--- src/core/directives/spinner.directive.js
+++ src/core/directives/spinner.directive.js
@@ -4,12 +4,31 @@
   yogaSpinner.$inject = [spinnerService];
 
   function yogaSpinner(spinnerService) {
-
+    
 
 
     return {
-     
-    };
+        restrict: "AE",
+        templateUrl: 'common/views/yoga-spinner.html',
+        link: function(scope, element, attributes) {
+
+            var shownType = element.css('display');
+
+            function hideElement() {
+                element.css('display', 'none');
+            }
+
+            scope.$on('hide spinner',hideElement);
+
+            scope.$on('show spinner', function() {
+                element.css('display', shownType);
+
+        });
+
+        hideElement();
+        
+        }
+    }
 
   };
 
