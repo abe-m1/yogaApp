431aa3f57fc583f0217b1813aa1b81f48974f3d0
abe <abestbet1@gmail.com>
2017-03-10 15:23:50 -0800

    boilerplate for blog service

---
+++ src/core/services/blog.service.js
@@ -0,0 +1,19 @@
+(function(module){
+    'use strict'
+
+    var blogService;
+    blogService.$inject = [];
+
+    function blogService() {
+
+        var exports = {
+         
+        }
+
+         
+
+        return exports
+    }
+
+    module.factory("blogService", blogService)
+})(angular.module('YogaApp.Services'))
