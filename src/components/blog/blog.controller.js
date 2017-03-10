4cb3b0ee881cb45be3ca61508cb760596ac3a922
abe <abestbet1@gmail.com>
2017-03-10 15:38:00 -0800

    functionality for creating and displaying blog

--- src/components/blog/blog.controller.js
+++ src/components/blog/blog.controller.js
@@ -3,12 +3,30 @@
   "use strict";
 
   var BlogCtrl;
-  BlogCtrl.$inject = [];
+  BlogCtrl.$inject = ['blogService'];
 
-  function BlogCtrl() {
+  function BlogCtrl(blogService) {
 
     var vm = this
     console.log('i am working')
+
+
+     vm.createBlog = function(data) {
+      
+      blogService.createBlog(data)
+      .then(function(res){
+        console.log('blog created', res)
+    
+      })
+    }
+
+    vm.getBlog = function(){
+        blogService.getBlog()
+      .then(function(res){
+        console.log('all blogs', res)
+    
+      })
+    } 
 
 
    
