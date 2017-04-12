e1542691e0a2cf00b336a332da0257238f624f99
abe <abestbet1@gmail.com>
2017-04-11 18:21:07 -0700

    change what we do to three items

--- src/components/abstract/abstract.controller.js
+++ src/components/abstract/abstract.controller.js
@@ -22,18 +22,6 @@
        3: {
          'text' : 'this will describe idea 3',
          'link' : 'http://lorempixel.com/400/200/nature/',
-       },
-       4: {
-         'text' : 'this will describe idea 4',
-         'link' : 'http://lorempixel.com/400/200/nature/',
-       },
-       5: {
-         'text' : 'this will describe idea 5',
-         'link' : 'http://lorempixel.com/400/200/nature/',
-       },
-       6: {
-         'text' : 'this will describe idea 6',
-         'link' : 'http://lorempixel.com/400/200/nature/',
        }
      };
    
